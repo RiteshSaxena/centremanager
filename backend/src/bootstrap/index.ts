b237f4cc32cd1a140fb0df8f2458f2310a9686ce
Ritesh Saxena <riteshsaxena55@outlook.com>
2023-12-13 21:05:03 +0530

    centre specific data

--- backend/src/bootstrap/index.ts
+++ backend/src/bootstrap/index.ts
@@ -6,4 +6,30 @@ import syncPermissions from './permissions';
 export default async ({ strapi }: { strapi: Strapi }) => {
   await initSettings(strapi);
   await syncPermissions(strapi);
+  await strapi.admin.services.permission.conditionProvider.register({
+    displayName: 'Is my centre data',
+    name: 'is-my-centre-data',
+    plugin: 'admin',
+    async handler(user) {
+      if (!user.roles[0].description) {
+        return false;
+      }
+
+      const centreId = parseInt(user.roles[0].description);
+
+      if (isNaN(centreId)) {
+        return false;
+      }
+
+      const center = await strapi.entityService.findOne('api::center.center', centreId);
+
+      if (!center) {
+        return false;
+      }
+
+      return {
+        center,
+      };
+    },
+  });
 };
