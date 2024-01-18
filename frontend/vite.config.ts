892b29f2cbff122649dd7710720e98a038d3c6f0
Ritesh Saxena <riteshsaxena55@outlook.com>
2024-01-18 16:02:55 +0530

    service worker update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,19 @@
 import { fileURLToPath, URL } from 'node:url';
+import { readFileSync } from 'node:fs';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { VitePWA } from 'vite-plugin-pwa';
+
+const manifest: any = JSON.parse(readFileSync('./manifest.json', 'utf-8'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      registerType: 'autoUpdate'
+      registerType: 'autoUpdate',
+      manifest
     })
   ],
   resolve: {
