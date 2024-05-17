516af34c656514c271309bd0eea10024e09a469c
Ritesh Saxena <riteshsaxena55@outlook.com>
2024-05-17 21:27:13 +0530

    add message for late

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router';
+import { onMounted } from 'vue';
 
 const route = useRoute();
+
+onMounted(() => {
+  document.querySelector('body')?.classList.add(APP_TYPE);
+});
 </script>
 
 <template>
