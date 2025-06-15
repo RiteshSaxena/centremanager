d757bd4ff359da8106e7fe2deb3d71d263e75cee
Ritesh Saxena <riteshsaxena55@outlook.com>
2025-06-16 01:03:21 +0530

    payment fixes / add staff for sign in

--- backend/config/cron-tasks.ts
+++ backend/config/cron-tasks.ts
@@ -122,7 +122,7 @@ export default {
             await strapi.entityService.update('api::child.child', child.id, {
               data: {
                 isDue: true,
-                dueAmount: currentDueAmount + child.paymentAmount ?? null,
+                dueAmount: currentDueAmount ? currentDueAmount + child.paymentAmount : null,
               },
             });
             continue;
--- frontend/src/views/Payment.vue
+++ frontend/src/views/Payment.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { computed, onMounted, ref, watch } from 'vue';
 import { debounce } from 'lodash';
+import DataTable from 'datatables.net-bs5';
 
 import type { SearchResult } from '@/types';
 
@@ -89,86 +90,105 @@ const addPayment = async () => {
 
 onMounted(async () => {
   await fetchPayments();
+  new DataTable('#due-students-table', {
+    paging: true,
+    pageLength: 25,
+    searching: false,
+    ordering: false,
+    info: false,
+    lengthChange: false,
+    autoWidth: false
+  });
+  new DataTable('#pay-history-table', {
+    paging: true,
+    pageLength: 25,
+    searching: false,
+    ordering: false,
+    info: false,
+    lengthChange: false,
+    autoWidth: false
+  });
 });
 </script>
 
 <template>
   <div class="mt-5">
     <h4 class="fw-bold">Payments Overdue</h4>
-    <div class="table-responsive">
-      <table class="table mt-3">
-        <thead class="table-danger">
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col" class="text-center">Due amount</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="align-middle" v-for="(record, index) in dueStudents" :key="record.id">
-            <th scope="row">{{ index + 1 }}</th>
-            <td>{{ record.firstName }} {{ record.lastName }}</td>
-            <td class="text-center">{{ record.dueAmount > 1 ? record.dueAmount : '-' }}</td>
-            <td class="text-center">
-              <button type="button" class="btn btn-info btn-sm" @click="openPaymentModal(record)">
-                Add Payment
-              </button>
-            </td>
-          </tr>
-          <tr v-if="!dueStudents.length">
-            <td class="text-center" colspan="4">No records found</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+    <table class="table mt-3" id="due-students-table">
+      <thead class="table-danger">
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col" class="text-center">Due amount</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="align-middle" v-for="(record, index) in dueStudents" :key="record.id">
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ record.firstName }} {{ record.lastName }}</td>
+          <td class="text-center">{{ record.dueAmount > 1 ? record.dueAmount : '-' }}</td>
+          <td class="text-center">
+            <button type="button" class="btn btn-info btn-sm" @click="openPaymentModal(record)">
+              Add Payment
+            </button>
+          </td>
+        </tr>
+        <tr v-if="!dueStudents.length">
+          <td class="text-center" colspan="4">No records found</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
   <h4 class="fw-bold mt-5">Payment History</h4>
-  <form class="mt-4 d-flex gap-2 align-items-center">
-    <div class="d-flex gap-1 report-search align-items-center">
-      <InputField v-model="search" placeholder="Enter student name to filter payment history" />
-      <button
-        v-if="search.trim().length"
-        type="button"
-        class="btn btn-secondary rounded-3"
-        @click="clearSearch"
-      >
-        <i class="fa-solid fa-xmark"></i>
-      </button>
-    </div>
-  </form>
-  <SearchResults
-    class="mt-3 report-search position-absolute shadow"
-    v-if="search.trim().length"
-    @onSelect="onSelectFromSearch"
-  />
+  <div class="position-relative">
+    <form class="mt-4 d-flex gap-2 align-items-center" @submit.prevent="fetchPayments()">
+      <div class="d-flex gap-1 report-search align-items-center">
+        <InputField v-model="search" placeholder="Enter student name to filter payment history" />
+        <button
+          v-if="search.trim().length"
+          type="button"
+          class="btn btn-secondary rounded-3"
+          @click="clearSearch"
+        >
+          <i class="fa-solid fa-xmark"></i>
+        </button>
+      </div>
+    </form>
+    <SearchResults
+      class="mt-3 report-search position-absolute shadow"
+      v-if="search.trim().length"
+      @onSelect="onSelectFromSearch"
+    />
+  </div>
   <p class="mt-4 mb-0" v-if="searchedId">
     Showing payment history for: <strong>{{ selectedName }}</strong>
   </p>
   <p class="mt-4 mb-0" v-if="loading">Loading...</p>
 
-  <div class="table-responsive">
-    <table class="table mt-4" v-if="paymentHistory.length">
-      <thead class="">
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Name</th>
-          <th scope="col">Amount</th>
-          <th scope="col">Payment Date</th>
-          <th scope="col">Notes</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(record, index) in paymentHistory" :key="record.id">
-          <th scope="row">{{ index + 1 }}</th>
-          <td>{{ record.child.firstName }} {{ record.child.lastName }}</td>
-          <td>{{ record.amount }}</td>
-          <td>{{ record.paymentDate }}</td>
-          <td>{{ record.notes }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+  <table class="table mt-4" id="pay-history-table">
+    <thead class="">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Amount</th>
+        <th scope="col">Payment Date</th>
+        <th scope="col">Notes</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(record, index) in paymentHistory" :key="record.id">
+        <th scope="row">{{ index + 1 }}</th>
+        <td>{{ record.child.firstName }} {{ record.child.lastName }}</td>
+        <td>{{ record.amount }}</td>
+        <td>{{ record.paymentDate }}</td>
+        <td>{{ record.notes }}</td>
+      </tr>
+      <tr v-if="!paymentHistory.length">
+        <td class="text-center" colspan="5">No records found</td>
+      </tr>
+    </tbody>
+  </table>
 
   <div v-if="studentId" class="mt-4">
     <button type="button" class="btn btn-info" @click="showAddPaymentModal = true">
