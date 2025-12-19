4a08f83d0c8549e1520a687d99bca9d2320a637a
hemanttechcurl <hemant.kumar@techcurl.com>
2025-12-19 15:53:23 +0530

    updated sign out modal

--- frontend/src/components/SignOutModal.vue
+++ frontend/src/components/SignOutModal.vue
@@ -114,6 +114,43 @@ const selectedName = computed(() => {
     :title="`Sign Out - ${selectedName}`"
     @close="emit('update:show', false)"
   >
+    <div class="w-100 fs-5 text-start d-flex flex-column gap-2">
+      <div class="row">
+        <div class="col-4"></div>
+        <div class="col-4"><strong>Score</strong></div>
+        <div class="col-4"><strong>Time <small>(M)</small></strong></div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <span><b>Maths</b></span>
+        </div>
+        <div class="col-4">
+          <span class="text-success">100%</span>
+        </div>
+        <div class="col-4">
+          <span>29</span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-4">
+          <span><b>Eng</b></span>
+        </div>
+        <div class="col-4">
+          <span class="text-warning"><b>-2</b></span>
+        </div>
+        <div class="col-4">
+          <span>32</span>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-12 mt-4 align-items-start justify-content-start d-flex">
+          <label><b>Feedback</b></label>
+        </div>
+        <div class="col-12">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore vel magnam magni similique minima sed, numquam autem consequatur sint perferendis dicta laborum hic aut voluptates quos iusto dolorem veritatis officiis?</p>
+        </div>
+      </div>
+    </div>
     <div v-if="qrMode" class="w-100 text-center my-5">
       <div class="spinner-border" role="status">
         <span class="visually-hidden">Loading...</span>
